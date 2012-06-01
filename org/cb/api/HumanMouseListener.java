bd3bf78cc0e314f5a19587a989ab368704c8257e
NissanNut <peletgun23@hotmail.com>
2012-06-01 00:25:17 -0500

    Renamed interface.

--- org/cb/api/HumanMouseListener.java
+++ org/cb/api/HumanMouseListener.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseEvent;
  * @author Troy
  *
  */
-public interface HumanMouseListener {
+public interface MouseListener {
 	public void mouseClicked(MouseEvent e);
 	public void mouseMoved(MouseEvent e);
 	public void mousePressed(MouseEvent e);
